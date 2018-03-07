e9f6d86974aed6597f9fee8eb3cbc816e36fca38
Olivier Sourie <Olivier.Sourie@howest.be>
2018-03-07 11:43:35 +0100

    BandLid Controller en 2 extra action methods

---
+++ DotNetCoreMVCOefeningenreeks1/Controllers/BandLidController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using MyHobbies;
+
+// For more information on enabling MVC for empty projects, visit http://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace DotNetCoreMVCOefeningenreeks1.Controllers
+{
+    public class BandLidController : Controller
+    {
+        private List<Band> bands;
+  
+        public BandLidController()
+        {
+            bands = BandInitializer.StartUp();
+        }
+    
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Lijst()
+        {
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public IActionResult Maak(string naam, int? jaar, Geslacht? geslacht, string band)
+        {
+            bands.Add(
+                new Band() {
+                    Naam = band ?? "Onbekend",
+                    Leden = new List<BandLid>()
+                    {
+                        new BandLid() {
+                            Naam = naam ?? "Onbekend",
+                            Geslacht = geslacht ?? Geslacht.Man }
+                    }
+                });
+            ViewBag.Bands = bands;
+            return View("Lijst");
+        }
+    }
+}

02be651cd5199ec357b8c14be95cafdcf2711ffd
Olivier Sourie <Olivier.Sourie@howest.be>
2018-03-07 11:31:25 +0100

    Band Controller met 3 nieuwe action methods

---
+++ DotNetCoreMVCOefeningenreeks1/Controllers/BandController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using MyHobbies;
+
+namespace DotNetCoreMVCOefeningenreeks1.Controllers
+{
+    public class BandController : Controller
+    {
+        private List<Band> bands;
+        public BandController()
+        {
+            bands = BandInitializer.StartUp();
+        }
+
+        // GET: /<controller>/
+        public ViewResult Index()
+        {
+            return View();
+        }
+
+        public ViewResult Lijst()
+        {
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public ViewResult LijstMetLeden()
+        {
+            ViewBag.Bands = bands;
+            return View();
+        }
+
+        public ViewResult Maak(string naam, int? jaar)
+        {
+            bands.Add(new Band()
+                {
+                    Naam = naam ?? "Onbekend",
+                    Jaar = jaar ?? DateTime.Now.Year
+                });
+            ViewBag.Bands = bands;
+            
+            return View("Lijst");
+        }
+    }
+}
---
+++ MyHobbies/BandInitializer.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHobbies
+{
+    public static class BandInitializer
+    {
+        static BandInitializer()
+        {
+  
+        }
+
+        public static  List<Band> StartUp() {
+            return new List<Band>() {
+                new Band() { Naam="The Beatles", Jaar=1960,
+                    Leden = new List<BandLid>()
+                        {
+                            new BandLid() { Naam="George", Geslacht=Geslacht.Man, Leeftijd=55, Levend=false },
+                            new BandLid() { Naam="Ringo", Geslacht=Geslacht.Man, Leeftijd=64, Levend=true },
+                            new BandLid() { Naam="Paul", Geslacht=Geslacht.Man, Leeftijd=76, Levend=true },
+                            new BandLid() { Naam="John", Geslacht=Geslacht.Man, Leeftijd=35, Levend=false }
+                        }
+                },
+                new Band() { Naam="Nicole & Hugo", Jaar=1302,
+                    Leden = new List<BandLid>()
+                        {
+                            new BandLid() { Naam="Nicole", Geslacht=Geslacht.Vrouw, Leeftijd=105, Levend=true },
+                            new BandLid() { Naam="Hugo", Geslacht=Geslacht.Man, Leeftijd=103, Levend=true },
+                        }
+                }
+            };
+
+        }
+    }
+}

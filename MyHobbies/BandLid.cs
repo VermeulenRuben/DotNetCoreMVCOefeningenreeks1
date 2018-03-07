175ed27669dcbcf8c1a832597c84b21bd44f18fd
Olivier Sourie <Olivier.Sourie@howest.be>
2018-03-07 10:52:06 +0100

    Band en Bandlid toegevoegd

---
+++ MyHobbies/BandLid.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHobbies
+{
+    public enum Geslacht
+    {
+        Man,
+        Vrouw
+    }
+
+    public class BandLid
+    {
+        public string Naam { get; set; }
+        public int Leeftijd { get; set; }
+        public Geslacht Geslacht { get; set; }
+        public bool Levend { get; set; }
+    }
+}
